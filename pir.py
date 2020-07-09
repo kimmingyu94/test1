1c9de7e9f042267b9a997b249c1a64af5b83fb67
kimmingyu94 <66464283+kimmingyu94@users.noreply.github.com>
2020-07-09 21:41:38 +0900

    Update pir.py

--- pir.py
+++ pir.py
@@ -1,17 +1,62 @@
 #!/usr/bin/python
-
+  
 import time
 import RPi.GPIO as GPIO
-
+import Adafruit_DHT
+import requests, json
+from influxdb import InfluxDBClient as influxdb
 GPIO.setmode (GPIO.BCM)
 GPIO.setup(4,GPIO.IN)
 
 def interrupt_fired(channel):
     print("interrupt Fired")
     print(channel)
-
-GPIO.add_event_detect(4,GPIO.FALLING,callback=interrupt_fired)
-
-while(True):
-    time.sleep(1)
-    print("timer fired")
+    a=5
+    data=[{
+        'measurement' : 'pir',
+        'tags':{
+            'visionUni':'2410',
+        },
+        'fields':{
+            'pir':a,
+        }
+    }]
+    client = None
+    try:
+        client = influxdb('localhost',8086,'root','root','pir')
+        except Exception as e:
+            print "Exception write" + str(e)
+        if client is not None:
+            try:
+                client.write_points(data)
+            except Exception as e:
+                print "Exception write" + str(e)
+            finally:
+                client.close()
+    print(a)
+    GPIO.add_event_detect(4,GPIO.FALLING,callback=interrupt_fired)
+    while(True):
+        time.sleep(1)
+        a=1
+        data=[{
+            'measurement' : 'pir',
+            'tags':{
+                'visionUni':'2410',
+            },
+            'fields':{
+                'pir':a,
+            }
+        }]
+        client = None
+        try:
+            client = influxdb('localhost',8086,'root','root','pir')
+        except Exception as e:
+            print "Exception write" + str(e)
+        if client is not None:
+            try:
+                client.write_points(data)
+            except Exception as e:
+                print "Exception write" + str(e)
+            finally:
+                client.close()
+       print("running influxdb ok")
